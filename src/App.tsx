2388ffdea412d82677ab456da031e186f11cbc53
keerthivasan0531 <113416871+keerthivasan0531@users.noreply.github.com>
2024-02-21 17:02:09 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import QueryPage from './components/QueryPage';
+
+
+
+function App() {
+
+  return (
+    <QueryPage/>
+  )
+}
+
+export default App
---
+++ src/components/QueryPage.tsx
@@ -0,0 +1,224 @@
+import { SetStateAction, useEffect, useState } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { InputTextarea } from 'primereact/inputtextarea'; // Import InputText component
+import { Dropdown } from "primereact/dropdown";
+import axios from "axios";
+
+function QueryPage() {
+  const data = [{ InputBox: "Sample Text." }];
+
+  const [inputText, setInputText] = useState("");
+  const [translatedText, setTranslatedText] = useState("");
+  const [targetLanguage, setTargetLanguage] = useState("Select a language");
+
+  const [orButtonClickCount, setORButtonClickCount] = useState(0);
+  const [orButtonLabel, setORButtonLabel] = useState("OR");
+  const [orButtonStyle, setORButtonStyle] = useState({});
+
+  const [andButtonClickCount, setANDButtonClickCount] = useState(0);
+  const [andButtonLabel, setANDButtonLabel] = useState("AND");
+  const [andButtonStyle, setANDButtonStyle] = useState({});
+
+  const [includeButtonClickCount, setIncludeButtonClickCount] = useState(0);
+  const [includeButtonLabel, setIncludeButtonLabel] = useState("INCLUDE");
+  const [includeButtonStyle, setIncludeButtonStyle] = useState({});
+
+  const [excludeButtonClickCount, setExcludeButtonClickCount] = useState(0);
+  const [excludeButtonLabel, setExcludeButtonLabel] = useState("EXCLUDE");
+  const [excludeButtonStyle, setExcludeButtonStyle] = useState({});
+
+  const languages = [
+    { code: "es", name: "Spanish" },
+    { code: "fr", name: "French" },
+    { code: "de", name: "German" },
+    { code: "it", name: "Italian" },
+    { code: "ja", name: "Japanese" },
+    { code: "pt", name: "Portuguese" },
+    { code: "ru", name: "Russian" },
+    { code: "ar", name: "Arabic" },
+    { code: "hi", name: "Hindi" },
+  ];
+
+  const handleButtonClick = (operation: string) => {
+    console.log(`Button clicked with operation: ${operation}`);
+
+    if (operation === "OR") {
+      setORButtonClickCount(orButtonClickCount + 1);
+
+      if (orButtonClickCount % 2 === 0) {
+        setORButtonLabel("OR🖐🏽");
+        setORButtonStyle({ border: "3px solid #BF55EC" });
+      } else {
+        setORButtonLabel("OR");
+        setORButtonStyle({ border: "3px solid #ff6666" });
+      }
+    } else if (operation === "AND") {
+      setANDButtonClickCount(andButtonClickCount + 1);
+
+      if (andButtonClickCount % 2 === 0) {
+        setANDButtonLabel("AND🖐🏽");
+        setANDButtonStyle({ border: "3px solid #BF55EC" });
+      } else {
+        setANDButtonLabel("AND");
+        setANDButtonStyle({ border: "3px solid #ff6666" });
+      }
+    } else if (operation === "INCLUDE") {
+      setIncludeButtonClickCount(includeButtonClickCount + 1);
+
+      if (includeButtonClickCount % 2 === 0) {
+        setIncludeButtonLabel("INCLUDE🖐🏽");
+        setIncludeButtonStyle({ border: "3px solid #BF55EC" });
+      } else {
+        setIncludeButtonLabel("INCLUDE");
+        setIncludeButtonStyle({ border: "3px solid #ff6666" });
+      }
+    } else if (operation === "EXCLUDE") {
+      setExcludeButtonClickCount(excludeButtonClickCount + 1);
+
+      if (excludeButtonClickCount % 2 === 0) {
+        setExcludeButtonLabel("EXCLUDE🖐🏽");
+        setExcludeButtonStyle({ border: "3px solid #BF55EC" });
+      } else {
+        setExcludeButtonLabel("EXCLUDE");
+        setExcludeButtonStyle({ border: "3px solid #ff6666" });
+      }
+    }
+  };
+  const handleLanguageChange = (e: { value: SetStateAction<string> }) => {
+    setTargetLanguage(e.value);
+  };
+  const handleInputChange = (e : any) => {
+    setInputText(e.target.value);
+  };
+
+  const translateText = async () => {
+    try {
+      const response = await axios.post("https://libretranslate.de/translate", {
+        q: inputText,
+        source: "en",
+        target: targetLanguage,
+      });
+      setTranslatedText(response.data.translatedText);
+    } catch (error) {
+      console.error("Error translating text:", error);
+    }
+  };
+
+  useEffect(() => {
+    translateText();
+  }, [targetLanguage]);
+
+  return (
+    <div className="">
+      <h1
+        style={{
+          backgroundColor: "#1087e8",
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "20px",
+          padding: "20px",
+        }}
+      >
+        Welcome to my Prime React React App
+      </h1>
+      <br />
+      <div className="card">
+        <DataTable
+          showGridlines
+          tableStyle={{ minHeight: "30rem" }}
+          value={data}
+        >
+          <Column
+            field="InputBox"
+            header="Search Test"
+            body={() => (
+              <div style={{ display: 'flex', alignItems: 'center'}}>
+              <InputTextarea
+                placeholder="Enter text to translate..."
+                value={inputText}
+                onChange={handleInputChange}
+                style={{marginRight:'10px'}} 
+              />
+              =
+              <InputTextarea
+              style={{marginLeft:'10px'}}
+                  value={translatedText}
+
+              />
+            </div>
+            )}
+          />
+          <Column
+            header="Operation"
+            body={() => (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <div style={{ display: "flex", marginBottom: "10px" }}>
+                  <Button
+                    label={orButtonLabel}
+                    className="p-button-raised p-button-rounded"
+                    style={{ marginRight: "10px", ...orButtonStyle }}
+                    outlined
+                    onClick={() => handleButtonClick("OR")}
+                  />
+                  <Button
+                    label={andButtonLabel}
+                    className="p-button-raised p-button-rounded"
+                    style={{ marginRight: "10px", ...andButtonStyle }}
+                    outlined
+                    onClick={() => handleButtonClick("AND")}
+                  />
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <Button
+                    label={includeButtonLabel}
+                    className="p-button-raised p-button-rounded"
+                    style={{ marginBottom: "10px", ...includeButtonStyle }}
+                    outlined
+                    onClick={() => handleButtonClick("INCLUDE")}
+                  />
+                  <Button
+                    label={excludeButtonLabel}
+                    className="p-button-raised p-button-rounded"
+                    style={{ marginBottom: "10px", ...excludeButtonStyle }}
+                    outlined
+                    onClick={() => handleButtonClick("EXCLUDE")}
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-end",
+                  }}
+                >
+                  <Dropdown
+                    value={targetLanguage}
+                    options={languages.map((lang) => ({
+                      label: lang.name,
+                      value: lang.code,
+                    }))}
+                    onChange={handleLanguageChange}
+                    placeholder="Select a language"
+                  />
+                </div>
+              </div>
+            )}
+            style={{ textAlign: "center" }}
+          ></Column>
+        </DataTable>
+      </div>
+    </div>
+  );
+}
+
+export default QueryPage;
